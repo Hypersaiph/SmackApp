a0050373830aed5823f3c84925aca45cf4d70914
geovani-mendoza-mojix-com <geovani.mendoza@mojix.com>
2018-10-11 17:45:43 -0400

    Jetpack implementation

--- app/src/main/java/ai/gvs/smackapp/MainActivity.kt
+++ app/src/main/java/ai/gvs/smackapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package ai.gvs.smackapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import ai.gvs.smackapp.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivitypatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
